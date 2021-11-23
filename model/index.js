d44ad17d8a1fb5004aeb8a9d9d61944ea77ba41e
nguyenson-255 <nguyenson-255@github.com>
2021-11-23 17:10:47 +0700

    Connection MySQL Success

--- model/index.js
+++ model/index.js
@@ -1,6 +1,5 @@
 const { Sequelize } = require('sequelize');
 
-
-const sequelize = new Sequelize(process.env.DB_CONNTECTION_STRING) // Example for postgres
+const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING) // Example for postgres
 
 module.exports = sequelize;
